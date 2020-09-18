1f1e015102d17434630ce18a5d5492b879a3d6da
zhaoyuntao <yuntao_zhao@163.com>
2020-09-18 15:15:18 +0800

    支持数据源切换，profile多环境

--- src/main/java/io/icefox/dmdemo/config/DataSourceConfig.java
+++ src/main/java/io/icefox/dmdemo/config/DataSourceConfig.java
@@ -1,13 +1,16 @@
 package io.icefox.dmdemo.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class DataSourceConfig {
-    
+
     @Value("${spring.datasource.url}")
     private String url;
 
@@ -26,15 +29,27 @@ public class DataSourceConfig {
     @Value("${spring.datasource.validation-query}")
     private String validationQuery;
 
+    @Value("${datasource.type}")
+    private String type;
+
     @Bean
-    DruidDataSource dataSource() {
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setUrl(url);
-        druidDataSource.setUsername(username);
-        druidDataSource.setPassword(password);
-        druidDataSource.setDriverClassName(driverClassName);
-        druidDataSource.setTestWhileIdle(testWhileIdle);
-        druidDataSource.setValidationQuery(validationQuery);
-        return druidDataSource;
+    DataSource dataSource() {
+        if (type.equals("druid")) {
+            DruidDataSource druidDataSource = new DruidDataSource();
+            druidDataSource.setUrl(url);
+            druidDataSource.setUsername(username);
+            druidDataSource.setPassword(password);
+            druidDataSource.setDriverClassName(driverClassName);
+            druidDataSource.setTestWhileIdle(testWhileIdle);
+            druidDataSource.setValidationQuery(validationQuery);
+            return druidDataSource;
+        } else {
+            HikariDataSource hikariDataSource = new HikariDataSource();
+            hikariDataSource.setJdbcUrl(url);
+            hikariDataSource.setUsername(username);
+            hikariDataSource.setPassword(password);
+            hikariDataSource.setDriverClassName(driverClassName);
+            return hikariDataSource;
+        }
     }
 }
